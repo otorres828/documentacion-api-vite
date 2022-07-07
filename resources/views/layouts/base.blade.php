361fdbfa0c795482b14ce67a5cf4ff49963b7ed0
otorres828 <olivertorres1997@gmail.com>
2022-07-08 01:34:52 +0800

    quitar mix

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css" rel="stylesheet">    
     <title>@yield('title')</title>
